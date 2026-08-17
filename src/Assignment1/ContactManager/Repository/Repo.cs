fdfa6ab6c8a3f29b5b5c84afaa96fe6da92a4362
27023-Akshreeya <akshreeya.thiyagarajan@solitontech.com>
2026-08-17 11:43:34 +0530

    Assignment 1 - Basic Contact manager application (#1)
    * feat:contact manager application
    
    * fix: modified solution file
    
    * fix: warning resolve
    
    * feat: Added feature on user coonsole to display error handled message
    
    * fix: editconfig file fix
    
    * fix: resolve peer review
    
    * fix: handled exception
    
    * fix : rename incomprehensible names,
    feat : add new feature to search all similar contacts
    
    * README with Contact Manager details
    
    Added overview, features, project structure, outcomes, and future improvements for the Contact Manager application.
    
    * Delete README1.md
    
    * style: update readme.md
    
    * fix : Resolved bugs
    
    * style : do intendation to output statements
    
    * feat : Add search by phone Number
    
    * fix : Refactored `Validator.cs` for cleaner logic and improved email validation using `System.Net.Mail.MailAddress`.
    
    * fix : add clean code
    
    * fix : update review change
    
    * fix : refactor code to follow proper naming convention

---
+++ src/Assignment1/ContactManager/Repository/Repo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using ContactManager.Models;
+using ContactManager.View;
+
+namespace ContactManager.Repository
+{
+    /// <summary>
+    /// This is the repo class
+    /// </summary>
+    public class Repo
+    {
+        private List<Contact> _contacts = new List<Contact>();
+
+        /// <summary>
+        /// This method adds object to the repository
+        /// </summary>
+        /// <param name="contact">This refers to the contacts</param>
+        public void AddContact(Contact contact)
+        {
+            this._contacts.Add(contact);
+        }
+
+        /// <summary>
+        /// this gets the list from repo to other classes
+        /// </summary>
+        /// <returns>returns the list</returns>
+        public List<Contact> GetAllContacts()
+        {
+            return this._contacts;
+        }
+
+        /// <summary>
+        /// Removes the contact with the specified unique identifier from the collection.
+        /// </summary>
+        /// <param name="id">The unique identifier of the contact to remove.</param>
+        public void RemoveContact(Guid id)
+        {
+            var existingContact = this._contacts.Find(c => c.Id == id);
+            if (existingContact != null)
+            {
+                this._contacts.Remove(existingContact);
+            }
+        }
+
+        /// <summary>
+        /// Updates the details of an existing contact identified by the specified unique identifier.
+        /// </summary>
+        /// <param name="updatedContact">The contact information used to update the existing contact.</param>
+        /// <param name="id">The unique identifier of the contact to update.</param>
+        public void UpdateContact(Contact updatedContact, Guid id)
+        {
+            var existingContact = this._contacts.Find(c => c.Id == id);
+            if (existingContact != null)
+            {
+                existingContact.Name = updatedContact.Name;
+                existingContact.PhoneNumber = updatedContact.PhoneNumber;
+                existingContact.EmailId = updatedContact.EmailId;
+                existingContact.Notes = updatedContact.Notes;
+            }
+        }
+    }
+}
