a7bbf2edf6db40af67da50b7819d3ac8cb94ebb8
27023-Akshreeya <akshreeya.thiyagarajan@solitontech.com>
2026-09-04 18:31:04 +0530

    feat : Add task 3

--- src/Assignment11/GarbageCollection/Program.cs
+++ src/Assignment11/GarbageCollection/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Assignments
+﻿using GarbageCollection.Domain;
+
+namespace Assignments
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Press Enter to START loop");
+            Console.ReadLine();
+            int loopCount = 10_000_000;
+            for (int i = 0; i < loopCount; i++)
+            {
+                new Structure { X = i };
+            }
+
+            Console.WriteLine("Loop finished. ");
+            Console.WriteLine("Triggering GC.Collect()");
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            Console.WriteLine("GC Complete.");
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
         }
     }
 }
