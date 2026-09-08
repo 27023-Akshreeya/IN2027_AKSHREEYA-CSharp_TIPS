fa8616ba411d2dfbb02e36077d45d9246cbf8fb6
27023-Akshreeya <akshreeya.thiyagarajan@solitontech.com>
2026-09-08 20:05:40 +0530

    feat : add optimized code

---
+++ src/Assignment12/MemoryOptimization/Program.cs
@@ -0,0 +1,20 @@
+﻿using MemoryOptimization;
+
+namespace Assignments
+{
+    /// <summary>
+    /// Contains the main execution logic for the memory optimization assignment.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Serves as the entry point for the application.
+        /// </summary>
+        /// <param name="args">An array of command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            MemoryEater me = new MemoryEater();
+            me.Allocate();
+        }
+    }
+}
