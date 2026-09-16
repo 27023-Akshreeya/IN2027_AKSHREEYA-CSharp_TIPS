45044a516ddba9777dbb5bdc2edc27229ce8cf7c
27023-Akshreeya <akshreeya.thiyagarajan@solitontech.com>
2026-09-16 20:05:59 +0530

    feat : add documentation

--- src/Assignment16/EventsAndDelegates/Notifer.cs
+++ src/Assignment16/EventsAndDelegates/Notifer.cs
@@ -1,11 +1,25 @@
 ﻿namespace EventsAndDelegates
 {
+    /// <summary>
+    /// di
+    /// </summary>
+    /// <param name="message">message</param>
     public delegate void Notify(string message);
 
+    /// <summary>
+    /// rdfdf
+    /// </summary>
     public class Notifer
     {
+        /// <summary>
+        /// d
+        /// </summary>
         public event Notify OnAction;
 
+        /// <summary>
+        /// d
+        /// </summary>
+        /// <param name="message">s</param>
         public void DisplayOnAction(string message) => OnAction?.Invoke(message);
     }
 }
--- src/Assignment16/EventsAndDelegates/Program.cs
+++ src/Assignment16/EventsAndDelegates/Program.cs
@@ -1,7 +1,14 @@
 ﻿namespace EventsAndDelegates
 {
+    /// <summary>
+    /// j
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// lj
+        /// </summary>
+        /// <param name="args">l</param>
         public static void Main(string[] args)
         {
             var notifer = new Notifer();
@@ -10,6 +17,10 @@
             notifer.OnAction -= PrintMessageToConsole;
         }
 
+        /// <summary>
+        /// hj
+        /// </summary>
+        /// <param name="message">f</param>
         public static void PrintMessageToConsole(string message)
         {
             Console.WriteLine($"the recieved message is : {message}");
