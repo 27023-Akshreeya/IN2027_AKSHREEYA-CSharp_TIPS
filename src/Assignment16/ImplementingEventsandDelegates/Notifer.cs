b7ec7d2f8113ca17e565e5bfdd339c91670bbe75
27023-Akshreeya <akshreeya.thiyagarajan@solitontech.com>
2026-09-16 14:06:11 +0530

    feat : add task 1

---
+++ src/Assignment16/ImplementingEventsandDelegates/Notifer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementingEventsandDelegates
+{
+    public delegate void Notify(string message);
+    public class Notifer
+    {
+        public event Notify OnAction;
+
+        public void DisplayOnAction(string message) => OnAction?.Invoke(message);
+    }
+}
---
+++ src/Assignment16/ImplementingEventsandDelegates/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ImplementingEventsandDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var notifer = new Notifer();
+            notifer.OnAction += PrintMessageToConsole;
+            notifer.DisplayOnAction("Hello world");
+            notifer.OnAction -= PrintMessageToConsole;
+        }
+
+        static void PrintMessageToConsole(string message)
+        {
+            Console.WriteLine($"the recieved message is : {message}");
+        }
+    }
+}
