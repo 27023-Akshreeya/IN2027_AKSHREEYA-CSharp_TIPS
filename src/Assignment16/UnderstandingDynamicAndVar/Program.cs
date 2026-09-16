15f929a87d350650ca5ee05823488c40e48913a6
27023-Akshreeya <akshreeya.thiyagarajan@solitontech.com>
2026-09-16 15:04:11 +0530

    feat : add task 2 implementation

--- src/Assignment16/UnderstandingDynamicAndVar/Program.cs
+++ src/Assignment16/UnderstandingDynamicAndVar/Program.cs
@@ -4,12 +4,19 @@
     {
         static void Main(string[] args)
         {
-            var integer = 110;
-            var word = "Hello";
+            var value = "Hello World!";
+            Console.WriteLine($"Understanding var keyword\nValue before reassigning : {value}" +
+                    $"\nType before reassigning : {value.GetType()}");
 
-            dynamic dynamicInterger = 123;
-            dynamic dynamicWord = "sd";
-            dynamicInterger = "hi";
+            // value = 123; //this throws an compile time exception
+            // This is because var locks the data type of the variable
+            // to the real data type behind the scenes, hence cant be changed
+            dynamic dynamicValue = 12345;
+            Console.WriteLine($"Understanding dynamic keyword\nValue before reassigning : {dynamicValue}" +
+                    $"\nType before reassigning : {dynamicValue?.GetType()}");
+            dynamicValue = "lorem ipsum";
+            Console.WriteLine($"Value after reassigning : {dynamicValue}" +
+                    $"\nType after reassigning : {dynamicValue?.GetType()}");
         }
     }
 }
