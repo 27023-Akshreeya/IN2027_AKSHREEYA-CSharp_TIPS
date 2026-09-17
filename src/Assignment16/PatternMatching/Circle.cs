f620c7142a9252ff228d2e73ec700633116123a4
27023-Akshreeya <akshreeya.thiyagarajan@solitontech.com>
2026-09-17 19:59:02 +0530

    feat : add task 7 implementation

---
+++ src/Assignment16/PatternMatching/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PatternMatching;
+
+/// <summary>
+/// Represents a circle shape.
+/// </summary>
+public class Circle : Shape
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Circle"/> class.
+    /// Initializes a circle with the given radius.
+    /// </summary>
+    /// <param name="radius">Circle radius.</param>
+    public Circle(double radius)
+        : base("Circle")
+    {
+        this.Radius = radius;
+    }
+
+    /// <summary>
+    /// Gets or sets the circle radius.
+    /// </summary>
+    /// <value> The circle radius.</placeholder>
+    /// </value>
+    public double Radius { get; set; }
+
+    /// <summary>
+    /// Calculates the circle area.
+    /// </summary>
+    /// <returns>Circle area.</returns>
+    public override double CalculateArea()
+    {
+        return Math.PI * this.Radius * this.Radius;
+    }
+}
---
+++ src/Assignment16/PatternMatching/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using PatternMatching;
+
+namespace Assignments;
+
+/// <summary>
+/// Provides the entry point and shape detail display logic for the application.
+/// </summary>
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Shape> shapes = new List<Shape>
+        {
+            new Circle(5),
+            new Rectangle(10, 5),
+            new Triangle(8, 4),
+            null,
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            DisplayShapeDetails(shape);
+        }
+
+        var square = new Square(6);
+
+        // DisplayShapeDetails(square);
+        // this will throw a compile time error stating that "Argument 1: cannot convert from 'PatternMatching.Square' to 'PatternMatching.Shape'"
+    }
+
+    private static void DisplayShapeDetails(Shape shape)
+    {
+        switch (shape)
+        {
+            case null:
+                Console.WriteLine("Shape is null.");
+                break;
+            case Circle circle:
+                Console.WriteLine($"Shape: Circle\nRadius: {circle.Radius}\nArea: {circle.CalculateArea()}");
+                break;
+            case Rectangle rectangle:
+                Console.WriteLine($"Shape: Rectangle\nLength: {rectangle.Length}\nWidth: {rectangle.Width}\nArea: {rectangle.CalculateArea()}");
+                break;
+            case Triangle triangle:
+                Console.WriteLine($"Shape: Triangle\nBase: {triangle.Base}\nHeight: {triangle.Height}\nArea: {triangle.CalculateArea()}");
+                break;
+            default:
+                Console.WriteLine("Unknown shape.");
+                break;
+        }
+    }
+}
