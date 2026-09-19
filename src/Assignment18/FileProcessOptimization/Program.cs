74bf5650b922b2e3e423ccf54a5e3791288f5f35
27023-Akshreeya <akshreeya.thiyagarajan@solitontech.com>
2026-09-19 17:45:28 +0530

    feat : add implementation for task 3

---
+++ src/Assignment18/FileProcessOptimization/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace FileProcessOptimization
+{
+    /// <summary>
+    /// Provides file write and read operations using a file stream.
+    /// </summary>
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                string path = "DummyFile.txt";
+                string data = "This is some test data";
+                byte[] writeBuffer = Encoding.ASCII.GetBytes(data);
+                using (FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
+                {
+                    fileStream.Write(writeBuffer, 0, writeBuffer.Length);
+                }
+
+                using (FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        string textChunk = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                        Console.Write(textChunk);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
