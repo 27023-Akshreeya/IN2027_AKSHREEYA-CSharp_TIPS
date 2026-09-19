3fdedf8c274b18448561f628adefb9dfd0460ade
27023-Akshreeya <akshreeya.thiyagarajan@solitontech.com>
2026-09-19 18:44:39 +0530

    feat : add implementation for task 4

---
+++ src/Assignment15/LoggingSystem/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace LoggingSystem
+{
+    public static class Logger
+    {
+        private static readonly object LockObject = new object();
+
+        public static void LogError(string userName, string errorMessage)
+        {
+            string fileName = userName + "_errors.txt";
+            lock (LockObject)
+            {
+                using (StreamWriter writer = new StreamWriter(fileName, true))
+                {
+                    writer.WriteLine(DateTime.Now + " - " + errorMessage);
+                }
+            }
+        }
+    }
+}
---
+++ src/Assignment15/LoggingSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using LoggingSystem;
+
+namespace Assignments
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            List<Task> tasks = new List<Task>();
+            for (int user = 1; user <= 5; user++)
+            {
+                tasks.Add(Task.Run(() => Logger.LogError("User" + user, "Error occurred")));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("All users logged successfully.");
+        }
+    }
+}
