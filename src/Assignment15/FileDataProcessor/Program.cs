0b5eada927c9d471c9dd49403f500d9aaf7734c8
27023-Akshreeya <akshreeya.thiyagarajan@solitontech.com>
2026-09-19 19:17:46 +0530

    fix : add task 1 files

---
+++ src/Assignment15/FileDataProcessor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FileDataProcessor;
+
+namespace Assignments;
+
+/// <summary>
+/// Entry point for the application that manages file creation, reading, and data processing operations.
+/// </summary>
+public class Program
+{
+    private const string SourceFilePath = "Source.txt";
+    private const string DestinationFile = "Copy.txt";
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Creating a large file");
+        var fileWriter = new FileWriter(SourceFilePath);
+        if (fileWriter.CreateLargeFile())
+        {
+            Console.WriteLine("Dummy file generation completed successfully.\n");
+        }
+        else
+        {
+            Console.WriteLine("File exits and already contains a size more than 1GB");
+        }
+
+        var reader = new FileReader(SourceFilePath);
+        Console.WriteLine($"FileStream Read Time: {reader.ReadWithFileStream()} ms");
+        Console.WriteLine($"BufferedStream Read Time: {reader.ReadWithBufferedStream()} ms");
+        Console.WriteLine($"Time taken to process data: {reader.ProcessData(DestinationFile)}");
+    }
+}
