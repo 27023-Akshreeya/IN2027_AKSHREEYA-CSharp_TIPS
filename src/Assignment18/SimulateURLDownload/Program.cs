8d310ee0cce6b63cb5c8bcb35b40a8749d88b369
27023-Akshreeya <akshreeya.thiyagarajan@solitontech.com>
2026-09-23 19:24:03 +0530

    feat : add implementation for task 1

---
+++ src/Assignment18/SimulateURLDownload/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace Assignments
+{
+    /// <summary>
+    /// This is the enty point to the code
+    /// </summary>
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            try
+            {
+                using HttpClient client = new HttpClient();
+                string url = @"https://www.geeksforgeeks.org/c-sharp/c-sharp-tutorial/";
+                string content = await client.GetStringAsync(url);
+                Console.WriteLine(content);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
